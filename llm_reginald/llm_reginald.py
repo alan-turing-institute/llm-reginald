f75aff6c0fad29f7ee692dc939e438ada7fe803f
Oliver Strickson <ostrickson@turing.ac.uk>
2024-06-12 11:36:08 +0100

    Basic interaction

--- llm_reginald/llm_reginald.py
+++ llm_reginald/llm_reginald.py
@@ -1,4 +1,5 @@
 import llm
+import httpx
 
 @llm.hookimpl
 def register_models(register):
@@ -7,6 +8,25 @@ def register_models(register):
 
 class Reginald(llm.Model):
     model_id = "reginald"
+    can_stream = False
+
+    default_host = "http://localhost:8000"
+
+    def _direct_message_endpoint(self, hostname=None):
+        if hostname is None:
+            hostname = self.default_host
+
+        return hostname + "/direct_message"
 
     def execute(self, prompt, stream, response, conversation):
-        return ["hello"]
+        message = prompt.prompt
+        user_id = ""
+
+        with httpx.Client() as client:
+            response = client.post(
+                self._direct_message_endpoint(),
+                json={"message": message, "user_id": user_id},
+                timeout=None
+            )
+            response.raise_for_status()
+            return response.json()['message']
