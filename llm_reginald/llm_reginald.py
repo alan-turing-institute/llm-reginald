0fde553c4e5fa05676c3da3a71b5f4b1f4bb3a64
Oliver Strickson <ostrickson@turing.ac.uk>
2024-06-12 18:11:17 +0100

    Support for new and continuation of existing conversations

--- llm_reginald/llm_reginald.py
+++ llm_reginald/llm_reginald.py
@@ -1,5 +1,6 @@
 import llm
 import httpx
+import uuid
 
 @llm.hookimpl
 def register_models(register):
@@ -19,14 +20,28 @@ class Reginald(llm.Model):
         return hostname + "/direct_message"
 
     def execute(self, prompt, stream, response, conversation):
+
         message = prompt.prompt
-        user_id = ""
+
+        # Reginald keeps a separate conversation history for each
+        # "user_id".  If 'conversation' is None (or doesn't have the
+        # user_id logged) start a new conversation by minting a new
+        # user_id history.  Otherwise, extract the logged user_id to
+        # continue that conversation.
+
+        try:
+            user_id = conversation.responses[0].response_json['user_id']
+        except (TypeError, AttributeError, KeyError, IndexError) as e:
+            user_id = str(uuid.uuid4().int)
 
         with httpx.Client() as client:
-            response = client.post(
+            reginald_reply = client.post(
                 self._direct_message_endpoint(),
                 json={"message": message, "user_id": user_id},
                 timeout=None
             )
-            response.raise_for_status()
-            return response.json()['message']
+
+        reginald_reply.raise_for_status()
+        yield reginald_reply.json()['message']
+
+        response.response_json = {'user_id': user_id}
